0942d6a26db9959d513cc06c103894f5b53cf935
BrianSavelkouls <ik@brain-storm.nl>
2015-04-30 07:29:44 +0200

    UUpdated genesis creation

--- src/main.cpp
+++ src/main.cpp
@@ -2819,8 +2819,8 @@ bool LoadBlockIndex(bool fAllowNew)
         printf("block.nTime = %u \n", block.nTime);
         printf("block.nNonce = %u \n", block.nNonce);
         printf("block.nBits = %u \n", block.nBits);
-        assert(block.hashMerkleRoot == uint256("0x5964e542044b261a1ebd944b982dbffe264712b5b973bad56ff383fc79a6b2c8"));
-        //block.print();
+        assert(block.hashMerkleRoot == uint256("0x4cb33b3b6a861dcbc685d3e614a9cafb945738d6833f182855679f2fad02057b"));
+        block.print();
         assert(block.GetHash() == (!fTestNet ? hashGenesisBlock : hashGenesisBlockTestNet));
         assert(block.CheckBlock());
 
